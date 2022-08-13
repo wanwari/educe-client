b2d33a8bcf5f169e130218b09196d9bf50b5d82a
Wiesa Anwari <wiesa@wanwari.ca>
2022-08-13 10:58:00 -0400

    setup: install & config basic map with leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { latLng } from "leaflet";
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 const App = () => {
+	const [mapCenter, setMapCenter] = useState(latLng(43.68, -79.38));
+
 	return (
 		<div>
-			<h1>Educe</h1>
+			<MapContainer
+				className="h-screen"
+				center={mapCenter}
+				zoom={12}
+				scrollWheelZoom={false}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={mapCenter}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 };
