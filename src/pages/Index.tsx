1ce43025cb2b295286940954fe93796e604abc91
Wiesa Anwari <wiesa@wanwari.ca>
2022-08-14 12:50:13 -0400

    refactor: move map rendering to index

---
+++ src/pages/Index.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Map from "../components/Map";
+import Points from "../components/Points";
+import data from "../tmpData.json";
+
+const Index = () => {
+	const [points, setPoints] = useState<any>();
+
+	useEffect(() => {
+		setPoints(data);
+	}, []);
+
+	return (
+		<div>
+			<Map>
+				<Points coordinates={points} />
+			</Map>
+		</div>
+	);
+};
+
+export default Index;
