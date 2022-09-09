83c4486fa4544c0efcd7f51c2a666215309cb0ef
Wiesa Anwari <wiesa@wanwari.ca>
2022-09-09 13:57:17 -0400

    feat: customize point icon

--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -2,14 +2,22 @@ import { Marker } from "react-leaflet";
 import { useDispatch } from "react-redux";
 import { setActivePoint } from "../state/slices/activePointSlice";
 import { setShowTooltip } from "../state/slices/showTooltipSlice";
+import { icon } from "leaflet";
+import point from "../img/b.png";
 
 const Point = (props: any) => {
 	const dispatch = useDispatch();
+
+	const customIcon = icon({
+		iconUrl: point,
+		iconSize: [35, 35],
+	});
 
 	return (
 		<Marker
 			key={props.point.id}
 			position={[props.point.latitude, props.point.longitude]}
+			icon={customIcon}
 			eventHandlers={{
 				click: (e) => {
 					dispatch(setActivePoint({ ...props.point }));
