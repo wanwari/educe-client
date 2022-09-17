8743be70ea5896dc6b856d4b14bd3081c1fa4259
Wiesa Anwari <wiesa@wanwari.ca>
2022-09-17 19:25:55 -0400

    fix: pan to clicked points using new state library

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,19 +1,28 @@
+import { useAtom } from "jotai";
 import { latLng } from "leaflet";
 import { PointArrayInterface } from "../Interfaces";
+import { activePoint } from "../state/store";
 import { MapPanner } from "./MapControls";
 import Point from "./Point";
 
 const Points = (props: { coordinates: PointArrayInterface }) => {
+	const [aPoint] = useAtom(activePoint);
+
 	return (
 		<div>
 			{props.coordinates.mapPoints &&
 				props.coordinates.mapPoints.map((point) => {
 					return (
 						<div key={point.id}>
-							<MapPanner
-								center={latLng([0, 0])}
-								initial={false}
-							/>
+							{aPoint && (
+								<MapPanner
+									center={latLng([
+										aPoint!.latitude,
+										aPoint!.longitude,
+									])}
+									initial={false}
+								/>
+							)}
 							<Point point={point} />
 						</div>
 					);
