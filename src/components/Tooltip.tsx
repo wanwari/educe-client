21dfc276fdc65c1308fac00a26ae20541da1acc2
Wiesa Anwari <wiesa@wanwari.ca>
2022-09-18 20:10:49 -0400

    style: tooltip v2

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from "jotai";
+import { useEffect } from "react";
 import {
 	ReactCompareSlider,
 	ReactCompareSliderImage,
@@ -13,9 +14,9 @@ const ToolTip = (props: { data: PointInterface }) => {
 	return (
 		<div>
 			{showTooltip && (
-				<div className="absolute bottom-10 left-5 right-5 bg-sky-100 md:bottom-auto md:left-auto md:top-10 md:right-10 md:w-3/6 lg:w-2/6 rounded-2xl text-stone-700 shadow-2xl px-4 py-2">
-					<div className="flex flex-row py-2 justify-between">
-						Learn More
+				<div className="absolute bottom-10 left-5 right-5 md:bottom-auto md:left-auto md:top-10 md:right-10 md:w-3/6 lg:w-2/6 rounded-2xl shadow-2xl ">
+					<div className="bg-zinc-700 text-white rounded-t-lg p-4 text-xl flex flex-row justify-between font-bold">
+						{props.data.name}
 						<button
 							onClick={() => {
 								setShowTooltip(false);
@@ -24,7 +25,13 @@ const ToolTip = (props: { data: PointInterface }) => {
 							<AiOutlineClose />
 						</button>
 					</div>
-					<div className="">
+					<div className="rounded-b-lg relative">
+						<p className="absolute top-2 left-2 z-20 text-white font-bold text-xl bg-slate-500 p-1 rounded">
+							{props.data.imageOneDate}
+						</p>
+						<p className="absolute top-2 right-2 z-20 text-white font-bold text-xl bg-slate-500 p-1 rounded">
+							{props.data.imageTwoDate}
+						</p>
 						<ReactCompareSlider
 							itemOne={
 								<ReactCompareSliderImage
@@ -40,18 +47,16 @@ const ToolTip = (props: { data: PointInterface }) => {
 							}
 						/>
 					</div>
-					<div className="flex flex-row justify-between italic text-sm">
-						<p>{props.data.imageOneDate}</p>
-						<p>{props.data.imageTwoDate}</p>
+					<div className="bg-zinc-700 rounded-b-lg p-4 text-lg flex align-middle justify-between">
+						{props.data.description && (
+							<p className="text-white text-sm grow-0 pr-4">
+								{props.data.description}
+							</p>
+						)}
+						<button className="text-white border-2 rounded-md px-8 py-2 border-blue-400 w-64 hover:text-blue-400  flex-1 min-w-fit">
+							Learn More
+						</button>
 					</div>
-					<h1 className="py-2 text-lg font-bold text-center text-black">
-						{props.data.name}
-					</h1>
-					{props.data.description && (
-						<h1 className="hidden md:block px-12 pb-4">
-							{props.data.description}
-						</h1>
-					)}
 				</div>
 			)}
 		</div>
